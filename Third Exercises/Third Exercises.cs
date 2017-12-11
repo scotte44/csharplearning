41486e9e4d840a79b2bce46741904f236dbc1d8e
Scott Easton <scotteaston44@hotmail.com>
2017-12-11 18:02:40 +0000

    2/4 Exercises Currently Complete.

--- Third Exercises/Third Exercises.cs
+++ Third Exercises/Third Exercises.cs
@@ -10,9 +10,72 @@ namespace Third_Exercises
     {
         static void Main(string[] args)
         {
+            {
 
+                // Exercise 10
+                // User enters numbers separated by '-'. Work out if the numbers are sequential, and display "Consecutive" or "Not Consecutive".
 
+                Console.WriteLine("Enter a Series of Numbers Seperated via hyphen: ");
+                var input = Console.ReadLine();
 
+                var numbers = new List<int>();
+                foreach (var number in input.Split('-'))
+                    numbers.Add(Convert.ToInt32(number));
+
+                numbers.Sort();
+
+                var isSequential = true;
+                for (var i = 1; i < numbers.Count; i++)
+                {
+                    if (numbers[i] != numbers[i - 1] + 1)    // Here ( i + 1) - 1 doesn't work. Work out why.
+                    {
+                        isSequential = false;
+                        break;
+                    }
+                }
+
+                var message = isSequential ? "Consecutive" : "Not Consecutive";
+                Console.WriteLine(message);
+
+            }
+
+            {
+
+                // Exercise 11
+                // User enters a few numbers sep. by hyphen. If the users presses enter, without input, exit. Otherwise check for dups and display Duplicates.
+
+                 
+
+                Console.WriteLine("Enter a Series of Numbers separated by a hyphen, or Press Enter to Exit: ");
+                var input = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(input))
+                return;
+
+                var numbers = new List<int>();
+                foreach (var number in input.Split('-'))
+                numbers.Add(Convert.ToInt32(number));
+                
+
+                var uniques = new List<int>();
+                var isDuplicate = false;
+                foreach (var number in numbers)
+                {
+                    if (!uniques.Contains(number))
+                        uniques.Add(number);
+                    else
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+
+                if (isDuplicate)
+                    Console.WriteLine("Duplicate");
+
+                // Initially used While, which meant the program didn't terminate, and asked for continueous streams of input, until Enter Pressed. Still showed "Duplicate".
+                // This is a better method of doing this. 
+            }
         }
     }
 }
